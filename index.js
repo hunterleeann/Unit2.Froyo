7303e045327b3549d9f5ded8c590da87921db665
Ignacio <iggyjurado45@gmail.com>
2024-10-17 21:56:37 -0400

    froyo

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const froyo = {
-    vanilla: 3,
-    coffee: 2,
-    strawberry: 1,
-};
+// const froyo = {
+//   vanilla: 3,
+//   coffee: 2,
+//   strawberry: 1,
+// };
 
-console.log(froyo.vanilla)
+//console.log(froyo.vanilla);
+//console.log(froyo.coffee);
+//console.log(froyo.strawberry);
+
+const userInputString = prompt("Please select some flavor.", "Vanilla");
+
+const stringArray = userInputString.split(",");
+
+const count = {};
+for (const element of stringArray) {
+  if (count[element]) {
+    count[element] += 1;
+  } else {
+    count[element] = 1;
+  }
+}
+console.log(count);
+// console.log(count);
+// console.log();
+// console.log();
+//
